ff10d03450d57d9113fe2ab31b4a1f5bc4ed82b9
Elias Hawa <elias.hawa2468@gmail.com>
2023-09-16 10:12:47 -0400

    Configure to receive data over serial

--- src/sender.js
+++ src/sender.js
@@ -1,15 +1,13 @@
-console.clear();
 require("dotenv").config();
 const {
-  AccountId,
-  PrivateKey,
   Client,
-  TopicCreateTransaction,
-  TopicMessageQuery,
   TopicMessageSubmitTransaction,
 } = require("@hashgraph/sdk");
+const { SerialPort, ReadlineParser } = require('serialport')
 
-// Grab the OPERATOR_ID and OPERATOR_KEY from the .env file
+console.clear();
+
+// Grab the OPERATOR_ID, OPERATOR_KEY and TOPIC_ID from the .env file
 const myAccountId = process.env.MY_ACCOUNT_ID;
 const myPrivateKey = process.env.MY_PRIVATE_KEY;
 const topicId = process.env.TOPIC_ID;
@@ -20,23 +18,45 @@ const client = Client.forTestnet();
 // Set the operator account ID and operator private key
 client.setOperator(myAccountId, myPrivateKey);
 
-async function submitFirstMessage() {
-  while (true){
-    // user input js
-    console.log("Waiting 10 seconds")
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+// Create a new SerialPort instance
+const portName = 'COM7'; 
+const port = new SerialPort({
+  path: portName,
+  baudRate: 9600, // Set the baud rate of your device
+});
 
-    // Send message to topic
-    let sendResponse = await new TopicMessageSubmitTransaction({
-      topicId: topicId,
-      message: "Hello, HCS!",
-    }).execute(client);
-    const getReceipt = await sendResponse.getReceipt(client);
+// Create a parser to read lines from the serial port
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
-    // Get the status of the transaction
-    const transactionStatus = getReceipt.status;
-    console.log("The message transaction status: " + transactionStatus.toString());
-  }  
-}
+// Open the serial port
+port.on('open', () => {
+  console.log(`Serial port ${portName} is open.`);
+});
 
-submitFirstMessage();
+// Read data from the serial port
+parser.on('data', async (data) => {
+  console.log(`Received data: ${data}`);
+  // Send message to topic
+  let sendResponse = await new TopicMessageSubmitTransaction({
+    topicId: topicId,
+    message: `${data}`,
+  }).execute(client);
+  const getReceipt = await sendResponse.getReceipt(client);
+
+  // Get the status of the transaction
+  const transactionStatus = getReceipt.status;
+  console.log("The message transaction status: " + transactionStatus.toString());
+});
+
+// Handle errors
+port.on('error', (err) => {
+  console.error('Error:', err.message);
+});
+
+// Close the serial port when the script exits (Ctrl+C)
+process.on('SIGINT', () => {
+  port.close(() => {
+    console.log(`Serial port ${portName} is closed.`);
+    client.close()
+  });
+});
