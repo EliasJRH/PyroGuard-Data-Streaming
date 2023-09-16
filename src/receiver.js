d4945515682f7b795ff77785ff45bed5b051ac12
Elias Hawa <elias.hawa2468@gmail.com>
2023-09-16 10:14:52 -0400

    Only change topic id if one doesn't exist

--- src/receiver.js
+++ src/receiver.js
@@ -21,40 +21,47 @@ const client = Client.forTestnet();
 client.setOperator(myAccountId, myPrivateKey);
 
 async function receiveMessages() {
-  // Create a new topic
-  let txResponse = await new TopicCreateTransaction().execute(client);
+  let topicId;
 
-  // Grab the newly generated topic ID
-  let receipt = await txResponse.getReceipt(client);
-  let topicId = receipt.topicId;
+  // If no topic exists, create a new topic
+  if (!process.env.TOPIC_ID) {
+    // Create a new topic
+    let txResponse = await new TopicCreateTransaction().execute(client);
 
-  let fileContents = [];
+    // Grab the newly generated topic ID
+    let receipt = await txResponse.getReceipt(client);
+    topicId = receipt.topicId;
 
-  fs.readFile(".env", 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    fileContents = data.split("\n")
-    if (fileContents.length == 2){
-      fileContents.push(`TOPIC_ID="${topicId}"`)
-    }else{
-      fileContents[2]=`TOPIC_ID="${topicId}"`
-    }
-  });
+    let fileContents = [];
 
-  // Wait 5 seconds between consensus topic creation and subscription creation
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  // This is a hack because I don't have a better way to pass topicId to sender.js
-  fs.writeFile(".env", fileContents.join("\n"), (err)=>{
-    if (err) {
-      console.error(err);
-      return;
-    }
-  })
+    fs.readFile(".env", 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      fileContents = data.split("\n")
+      if (fileContents.length == 2){
+        fileContents.push(`TOPIC_ID="${topicId}"`)
+      }else{
+        fileContents[2]=`TOPIC_ID="${topicId}"`
+      }
+    });
+
+    // Wait 5 seconds between consensus topic creation and subscription creation
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // This is a hack because I don't have a better way to pass topicId to sender.js
+    fs.writeFile(".env", fileContents.join("\n"), (err)=>{
+      if (err) {
+        console.error(err);
+        return;
+      }
+    })
+  } else {
+    topicId = process.env.TOPIC_ID;
+  }
+  
   console.log("Ready to receive messages")
 
-  // Create the topic and starts listening
   new TopicMessageQuery()
     .setTopicId(topicId)
     .subscribe(client, null, (message) => {
